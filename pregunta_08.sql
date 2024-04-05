264d76ce76ea9af49463de73f8a407f76404f368
jlondonolo <jlondonolo@unal.edu.co>
2024-04-05 17:55:40 -0400

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,6 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT strftime('%Y', c23) AS "strftime('%Y',c23)", AVG(c21) 
-	FROM tbl2 GROUP BY 1;
-
+SELECT strftime('%Y', c23) as "strftime('%Y', c23)" , avg(c21)  FROM tbl2 GROUP BY 1;
